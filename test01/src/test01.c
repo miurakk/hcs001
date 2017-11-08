b46bcb1afe0dab062632fcc11aee48a08f1f414d
jinlong.ji <jinlong.ji@ustchcs.com>
2017-11-08 16:49:03 +0800

    add a string of "Hello Git" to the printf.

--- test01/src/test01.c
+++ test01/src/test01.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : test01.c
- Author      : 
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World, Hello Git!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
